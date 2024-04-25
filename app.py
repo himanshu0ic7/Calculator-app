e51987c714420e4966a50d26a322138eb3b9e2b5
Himanshu Kumar Saw <129617476+himanshu0ic7@users.noreply.github.com>
2024-04-25 07:35:17 +0530

    added area functionality

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from function import add,mul,sub
 from divide import divide
+from area import area
 def main():
     print('''
         1. Enter 1 for addition
         2. Enter 2 for subtraction
         3. Enter 3 for Multiplication
         4. Enter 4 for division
+        5. Enter 5 for area
 ''')
     choice=int(input("Enter your choice: "))
     a=int(input("Enter A: "))
@@ -18,6 +20,8 @@ def main():
         print(mul(a,b))
     elif choice==4:
         print(divide(a,b))
+    elif choice==5:
+        print(area(a,b))
 
 if __name__=="__main__":
     main()
