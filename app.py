5684bf2f83e16cd57e3f4d5fd2de4071453f6217
Himanshu Kumar Saw <129617476+himanshu0ic7@users.noreply.github.com>
2024-04-25 07:31:40 +0530

    Added a divide functionality

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from function import add,mul,sub
+from divide import divide
 def main():
     print('''
         1. Enter 1 for addition
         2. Enter 2 for subtraction
         3. Enter 3 for Multiplication
+        4. Enter 4 for division
 ''')
     choice=int(input("Enter your choice: "))
     a=int(input("Enter A: "))
@@ -14,6 +16,8 @@ def main():
         print(sub(a,b))
     elif choice==3:
         print(mul(a,b))
+    elif choice==4:
+        print(divide(a,b))
 
 if __name__=="__main__":
     main()
