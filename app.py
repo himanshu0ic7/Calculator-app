040ddc713b559c20091f195ade21695a1920d11c
Himanshu Kumar Saw <129617476+himanshu0ic7@users.noreply.github.com>
2024-04-25 07:37:06 +0530

    added a perimeter functionality

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from function import add,mul,sub
 from divide import divide
+from perimeter import perimeter
 def main():
     print('''
         1. Enter 1 for addition
         2. Enter 2 for subtraction
         3. Enter 3 for Multiplication
         4. Enter 4 for division
+        5. Enter 5 for perimeter
 ''')
     choice=int(input("Enter your choice: "))
     a=int(input("Enter A: "))
@@ -18,6 +20,8 @@ def main():
         print(mul(a,b))
     elif choice==4:
         print(divide(a,b))
+    elif choice==5:
+        print(perimeter(a,b))
 
 if __name__=="__main__":
     main()
